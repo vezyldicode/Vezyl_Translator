0a1b5b7c8f908110084ea5009272b0fbf23dae54
vezyldicode <nguyentuanviet300605@gmail.com>
2025-08-17 00:36:28 +0200

    v1.5.14 -release

--- build_package.bat
+++ build_package.bat
@@ -2,10 +2,10 @@
 setlocal
 
 REM 1. Đặt biến version
-set "version=1.5.3_beta"
+set "version=1.5.14"
 
 REM 2. Tạo thư mục đích
-set "dest=Vezyl_Translator_%version%"
+set "dest=v_%version%"
 if exist "%dest%" rmdir /s /q "%dest%"
 mkdir "%dest%"
 
@@ -18,14 +18,11 @@ REM 4. move thư mục _internal
 move "_internal" "%dest%\_internal"
 if exist "%dest%\_internal\resources" rmdir /s /q "%dest%\_internal\resources"
 if exist "%dest%\_internal\config" rmdir /s /q "%dest%\_internal\config"
-if exist "%dest%\_internal\VezylTranslatorElectron\local" rmdir /s /q "%dest%\_internal\VezylTranslatorElectron\local"
-if exist "%dest%\_internal\VezylTranslatorNeutron\local" rmdir /s /q "%dest%\_internal\VezylTranslatorNeutron\local"
-if exist "%dest%\_internal\VezylTranslatorProton\local" rmdir /s /q "%dest%\_internal\VezylTranslatorProton\local"
-
 
 REM 7. Copy thư mục resources, sau đó xóa thư mục con 'neveruse'
 xcopy "resources" "%dest%\resources" /E /I
 if exist "%dest%\resources\neveruse" rmdir /s /q "%dest%\resources\neveruse"
+if exist "%dest%\resources\marian_models" rmdir /s /q "%dest%\resources\marian_models"
 
 REM 8. Copy thư mục config
 xcopy "config" "%dest%\config" /E /I
